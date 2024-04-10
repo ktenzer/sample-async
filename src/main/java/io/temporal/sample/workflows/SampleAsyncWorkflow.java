3523f8c44da2c256c1722cc943dc4974ed4fd798
Keith Tenzer <ktenzer@keiths-mbp.lan>
2024-04-10 13:19:47 -0700

    added timer which if fires we pause workflow and await signal. Once we recieve signal we continue-as-new. In addition we expose query to show state of workflow if its paused or not.
    Signed-off-by: Keith Tenzer <ktenzer@keiths-mbp.lan>

--- src/main/java/io/temporal/sample/workflows/SampleAsyncWorkflow.java
+++ src/main/java/io/temporal/sample/workflows/SampleAsyncWorkflow.java
@@ -4,9 +4,17 @@ import io.temporal.sample.model.SampleInput;
 import io.temporal.sample.model.SampleResult;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
+import io.temporal.workflow.SignalMethod;
+import io.temporal.workflow.QueryMethod;
 
 @WorkflowInterface
 public interface SampleAsyncWorkflow {
     @WorkflowMethod
     SampleResult run(SampleInput input);
+
+    @QueryMethod
+    boolean isPaused();
+
+    @SignalMethod
+    void resume();   
 }
--- src/main/java/io/temporal/sample/workflows/SampleAsyncWorkflowImpl.java
+++ src/main/java/io/temporal/sample/workflows/SampleAsyncWorkflowImpl.java
@@ -10,6 +10,8 @@ import io.temporal.workflow.*;
 import org.slf4j.Logger;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @WorkflowImpl(taskQueues = "samplequeue")
 public class SampleAsyncWorkflowImpl implements SampleAsyncWorkflow {
@@ -18,13 +20,28 @@ public class SampleAsyncWorkflowImpl implements SampleAsyncWorkflow {
     private final SampleActivities activities = Workflow.newActivityStub(SampleActivities.class);
     private Promise<Void> activitiesPromise;
     Promise<Void> timerPromise;
+    private boolean awaitingPause = true;
 
     @Override
     public SampleResult run(SampleInput input) {
+        // Create cancellation scope for timer
+        CancellationScope timerCancellationScope =
+                Workflow.newCancellationScope(
+                        () -> {
+                            timerPromise = Workflow.newTimer(Duration.ofSeconds(input.getTimer()));
+                        });
+        timerCancellationScope.run();
+        // Create cancellation scope for activities
+        CancellationScope activityCancellationScope =
+                Workflow.newCancellationScope(
+                        () -> {
+                            activitiesPromise = Async.procedure(this::runActivities);
+                        });
+        activityCancellationScope.run();
+
         // Wait for timer and activities promises, whichever completes first
-        activitiesPromise = Async.procedure(this::runActivities);
         try {
-            Promise.allOf(activitiesPromise).get();
+            Promise.anyOf(timerPromise, activitiesPromise).get();
         } catch (ActivityFailure e) {
             // We need to handler ActivityFailure here as it will be delivered to workflow code in this .get() call
             // However we just log it as will handle later with activitiesPromise.getFailure
@@ -32,9 +49,43 @@ public class SampleAsyncWorkflowImpl implements SampleAsyncWorkflow {
             logger.warn("Activity failure: " + e.getMessage());
         }
 
+        // if our timer promise completed but activities are still running
+        if (timerPromise.isCompleted() && !activitiesPromise.isCompleted()) {
+            activityCancellationScope.cancel("timer fired");
 
-        return new SampleResult("Parent wf: result, no compensation initiated....");
+            // Upsert search attribute `paused` to true before blocking
+            Map<String, Object> searchAttributes = new HashMap<>();
+            searchAttributes.put("paused", true);
 
+            // Wait for the pause signal
+            Workflow.await(() -> !awaitingPause);
+
+            Workflow.continueAsNew(input);
+            return new SampleResult("Parent wf: result, continue-as-new");
+            // fail execution
+            //throw ApplicationFailure.newFailure("Workflow timeout, failing workflow", "TimerFired");
+
+
+        } else {
+            // cancel timer so TimerFired does not get delivered to our worker
+            // in case timer does fire before or at the time we want to complete execution
+            timerCancellationScope.cancel("activities completed/failed before timer");
+            if (activitiesPromise.getFailure() != null) {
+                return new SampleResult("Parent wf: result, compensation initiated...");
+            }
+            return new SampleResult("Parent wf: result, no compensation initiated....");
+        }
+    }
+    
+    @Override
+    public void resume() {
+        awaitingPause = false;
+    }
+
+    // Query method implementation
+    @Override
+    public boolean isPaused() {
+        return awaitingPause;
     }
 
     private void runActivities() {
